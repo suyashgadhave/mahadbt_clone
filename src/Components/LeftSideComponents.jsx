b390e7c751dea8b9bef826d7112762b0f677b2ee
Suyash Sanjay Gadhave <115340599+suyashgadhave@users.noreply.github.com>
2024-05-16 22:36:10 +0530

    Update LeftSideComponents.jsx

--- src/Components/LeftSideComponents.jsx
+++ src/Components/LeftSideComponents.jsx
@@ -5,10 +5,10 @@ const LeftSideComponent = () => {
   return (
     <div className="w-72 flex flex-col items-start mt-2">
       <div className="border border-gray-300 h-12 w-full">
-        <a href="#" className="py-2 px-4 block text-blue-500 hover:text-blue-700">Aadhaar Bank Link</a>
+        <a href="javascript:void(0)" className="py-2 px-4 block text-blue-500 hover:text-blue-700">Aadhaar Bank Link</a>
       </div>
       <div className="border border-gray-300 h-12 w-full">
-        <a href="#" className="py-2 px-4 block text-blue-500 hover:text-blue-700">Home</a>
+        <a href="javascript:void(0)" className="py-2 px-4 block text-blue-500 hover:text-blue-700">Home</a>
       </div>
       <div className="border border-gray-300 h-12 w-full">
         <Link to="/profile" className="py-2 px-4 block text-blue-500 hover:text-blue-700">Profile</Link>
@@ -17,25 +17,25 @@ const LeftSideComponent = () => {
         <Link to="/profile/all-scholarships" className="py-2 px-4 block text-blue-500 hover:text-blue-700">All Schemes</Link>
       </div>
       <div className="border border-gray-300 h-12 w-full">
-        <a href="#" className="py-2 px-4 block text-blue-500 hover:text-blue-700">My Applied Scheme</a>
+        <a href="javascript:void(0)" className="py-2 px-4 block text-blue-500 hover:text-blue-700">My Applied Scheme</a>
       </div>
       <div className="border border-gray-300 h-12 w-full">
-        <a href="#" className="py-2 px-4 block text-blue-500 hover:text-blue-700">My Cancelled Scheme</a>
+        <a href="javascript:void(0)" className="py-2 px-4 block text-blue-500 hover:text-blue-700">My Cancelled Scheme</a>
       </div>
       <div className="border border-gray-300 h-12 w-full">
-        <a href="#" className="py-2 px-4 block text-blue-500 hover:text-blue-700">Right To Give Up</a>
+        <a href="javascript:void(0)" className="py-2 px-4 block text-blue-500 hover:text-blue-700">Right To Give Up</a>
       </div>
       <div className="border border-gray-300 h-12 w-full">
-        <a href="#" className="py-2 px-4 block text-blue-500 hover:text-blue-700">Grievance/Suggestion Dashboard</a>
+        <a href="javascript:void(0)" className="py-2 px-4 block text-blue-500 hover:text-blue-700">Grievance/Suggestion Dashboard</a>
       </div>
       <div className="border border-gray-300 h-12 w-full">
-        <a href="#" className="py-2 px-4 block text-blue-500 hover:text-blue-700">Declaration Forms</a>
+        <a href="javascript:void(0)" className="py-2 px-4 block text-blue-500 hover:text-blue-700">Declaration Forms</a>
       </div>
       <div className="border border-gray-300 h-12 w-full">
-        <a href="#" className="py-2 px-4 block text-blue-500 hover:text-blue-700">Notification</a>
+        <a href="javascript:void(0)" className="py-2 px-4 block text-blue-500 hover:text-blue-700">Notification</a>
       </div>
       <div className="border border-gray-300 h-12 w-full">
-        <a href="#" className="py-2 px-4 block text-blue-500 hover:text-blue-700">My Applied Scheme History</a>
+        <a href="javascript:void(0)" className="py-2 px-4 block text-blue-500 hover:text-blue-700">My Applied Scheme History</a>
       </div>
     </div>
   );
